9a9a671a730b4f61eb8d0bd0b5690545fcb87a59
Nathan <80951962+Hakinathan@users.noreply.github.com>
2023-05-25 21:09:58 +0200

    Pagination view

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultView from './views/resultView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -35,7 +36,9 @@ const controlSearchResults = async function () {
     resultView.renderSpinner();
     await model.loadSearchResults(query);
 
-    resultView.render(model.state.search.results);
+    resultView.render(model.getSearchResultPage());
+
+    paginationView.render(model.state.search);
   } catch (error) {
     recipeView.renderError();
   }
---
+++ src/js/views/paginationView.js
@@ -0,0 +1,14 @@
+import View from './view';
+import icons from 'url:../../img/icons.svg';
+
+class PaginationView extends View {
+  _parentElement = document.querySelector('.pagination');
+
+  _generateMarkup() {
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
+  }
+}
+
+export default new PaginationView();
